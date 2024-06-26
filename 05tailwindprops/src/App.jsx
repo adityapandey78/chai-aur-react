97f1c9842cda511d152c252182ea4b03881860b4
adityapandey78 <adityapandey.2402@gmail.com>
2024-06-26 20:39:52 +0530

    .

--- 05tailwindprops/src/App.jsx
+++ 05tailwindprops/src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './components/card'
-
+import Card from '../src/components/Card'
 function App() {
   const [count, setCount] = useState(0)
   let myObject={
@@ -13,9 +12,7 @@ function App() {
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-2'>tailwind test</h1>
-      <Card channel="chaiAurcode" someObj={myObject}  />
-      <Card username="Aditya" />
-
+     <Card/>
     </>
   )
 }
