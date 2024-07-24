9841bc92deb76ec2f21109b0f7816cbea02cd3f0
adityapandey78 <adityapandey.2402@gmail.com>
2024-07-25 00:06:35 +0530

    .

--- 15GiphyClone/src/Components/Header.jsx
+++ 15GiphyClone/src/Components/Header.jsx
@@ -1,78 +1,81 @@
-import React,{useEffect, useState} from 'react'
-import { Link } from 'react-router-dom';
-import {HiEllipsisVertical, HiMiniBars3BottomRight} from "react-icons/hi2"
-import GifProvider, { GifState } from '../Context/Gif-context';
-
+import { useEffect, useState } from "react";
+import GifProvider, { GifState } from "../Context/Gif-context";
+import { Link } from "react-router-dom";
+import { HiEllipsisVertical, HiMiniBars3BottomRight } from "react-icons/hi2";
 
 const Header = () => {
-    const [category, setCategory] = useState([]);
-    const [showCategory, setShowCategory] = useState(false);
+  const [category, setCategory] = useState([]);
+  const [showCategory, setShowCategory] = useState(false);
 
-    const {gif,filter,setFilter,favourites}=GifState();
+  const { gif, filter, setFilter, favourites } = GifState();
 
-    const fetchGifCategories =async()=>{
-      const {data}=await gif.category();
-      setCategory(data);
-    };
+  const fetchGifCategories = async () => {
+    const res = await fetch("/categories.json");
+    const { data } = await res.json();
+    setCategory(data);
+  };
 
-    useEffect(()=>{
-      fetchGifCategories();
-    },[]);
+  useEffect(() => {
+    fetchGifCategories();
+  }, []);
 
   return (
     <nav>
-        <div className='relative flex gap-4 justify-between items-center mb-2'>
-            <Link to="/" className='flex gap-2'>
-            <img src="/logo.svg" alt="Giphy Logo" className='w-8' />
-            <h1 className='text-5xl font-bold tracking-tight cursor-pointer'>
+      <div className="relative flex gap-4 justify-between items-center mb-2">
+        <Link to={"/"} className="flex gap-2">
+          <img src="/logo.svg" alt="Giphy Logo" className="w-8" />
+          <h1 className="text-5xl font-bold tracking-tight cursor-pointer">
             GIPHY
-            </h1>
-            </Link>
-            
-            <div className='font-bold text-md flex gap-2 items-center'>
-              {/* Render Categories */}
-              {category?.slice(0,5).map((cat)=>{
-                return(
-                  <Link 
-                  key={cat.name}
-                  to={`/${cat.name.encoded}`}
-                  className='px-4 py-1 hover:gradient border-b-4 hidden lg:block'>
-                  {cat.name}</Link>
-                )
-              })}
+          </h1>
+        </Link>
 
-
-            <button onClick={()=>setShowCategory(!showCategory)}>
-                < HiEllipsisVertical 
-                size={35} 
-                className={`py-0.5 hover:gradient ${
-                  showCategory? "gradient":""
-                  } border-b-4 hidden lg:block`}
-                />
-            </button>
-            <div className='h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded'>
-              <Link to="favourites"> Favourite GIFs</Link>
-            </div>
-            <button>
-              <HiMiniBars3BottomRight className='text-sky-400 block lg:hidden'
-              size={30}/>
-            </button>
+        <div className="font-bold text-md flex gap-2 items-center">
+          {/* Render Categories */}
+          {category?.slice(0, 5).map((cat) => {
+            return (
+              <Link
+                key={cat.name}
+                to={`/${cat.name_encoded}`}
+                className="px-4 py-1 hover:gradient border-b-4 hidden lg:block"
+              >
+                {cat.name}
+              </Link>
+            );
+          })}
+          ;
+          <button onClick={() => setShowCategory(!showCategory)}>
+            <HiEllipsisVertical
+              size={35}
+              className={`py-0.5 hover:gradient ${
+                showCategory ? "gradient" : ""
+              } border-b-4 hidden lg:block`}
+            />
+          </button>
+          <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
+            <Link to="/favourites"> Favourite GIFs</Link>
           </div>
-
-         {showCategory && (
-          <div className='absolute right-0 top-14 px-10 pt-6 pb-9 w-full gradient z-20'>
-          <span>Categories</span>
-          <hr />
-          <div>
-            <Link className='font-bold '>Reactions</Link>
-          </div>
-          </div>
-        )}
+          <button>
+            <HiMiniBars3BottomRight
+              className="text-sky-400 block lg:hidden"
+              size={30}
+            />
+          </button>
         </div>
 
-        {/* {search} */}
-    </nav>
-  )
-}
+        {showCategory && (
+          <div className="absolute right-0 top-14 px-10 pt-6 pb-9 w-full gradient z-20">
+            <span>Categories</span>
+            <hr />
+            <div>
+              <Link className="font-bold ">Reactions</Link>
+            </div>
+          </div>
+        )}
+      </div>
 
-export default Header
+      {/* {search} */}
+    </nav>
+  );
+};
+
+export default Header;
--- 15GiphyClone/src/Context/Gif-context.jsx
+++ 15GiphyClone/src/Context/Gif-context.jsx
@@ -1,18 +1,21 @@
-import {GiphyFetch} from "@giphy/js-fetch-api";
-import {createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
+import { GiphyFetch } from '@giphy/js-fetch-api';
 
 const GifContext=createContext();
 
-const GifProvider=({Children})=>{
+const GifProvider=({children})=>{
     const [gifs, setGifs] = useState([]);
     const [filter, setFilter] = useState("gifs");
     const [favourites, setFavourites] = useState([]);
 
 
-    const gif=new GiphyFetch(import.meta.env.VITE_GIPHY_KEY); //now importing all the GIPHY data with an API Key
-
+    const gif=new GiphyFetch(import.meta.env.VITE_GIPHY_KEY);
+     //now importing all the GIPHY data with an API Key
+     if (!giphyKey) {
+        console.error('GIPHY API key is missing');
+      }
     return (<GifContext.Provider value={{gif,gifs,setGifs,filter,setFilter,favourites}}>
-        {Children}
+        {children}
     </GifContext.Provider>);
 };
 
